5ce6c86c49c4f108036c759d148ebb61cbd4f715
Tim Everett <tim.neil.everett@gmail.com>
2025-09-21 13:18:50 -0300

    chore: remove placehodelr feature

--- features/placeholder-feature/migrations/create_placeholder_feature.sql
+++
@@ -1,7 +0,0 @@
--- placeholder migration
--- if schema files are included in this feature
--- this file shoudl contain the code generated
--- when running `supabase db diff`
-
--- it can also other sql statements not captured by 
--- supabase db diff
